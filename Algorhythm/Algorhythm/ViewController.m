0abe66684df2b2011bf8383cd5bde3a021dddaf8
cookiegu <cookiegu.se@gmail.com>
2015-10-10 02:28:22 +0900

    changed backgroundColor and button Color when pressed button

--- Algorhythm/Algorhythm/ViewController.m
+++ Algorhythm/Algorhythm/ViewController.m
@@ -16,12 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.aButton setTitle:@"Press me!" forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    UIColor *redColor = [UIColor redColor];
+    [self.aButton setBackgroundColor:redColor];
+    self.view.backgroundColor = [UIColor orangeColor];
 }
 
 @end
