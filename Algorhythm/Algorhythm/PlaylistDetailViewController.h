810cabf9467751c7b255ed201b472cccd178d284
cookiegu <cookiegu.se@gmail.com>
2015-10-16 23:33:50 +0900

    created PlaylistDetailViewController

---
+++ Algorhythm/Algorhythm/PlaylistDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaylistDetailViewController.h
+//  Algorhythm
+//
+//  Created by Cookie Gu on 15/10/16.
+//  Copyright © 2015年 Cookie Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaylistDetailViewController : UIViewController
+
+@end
